e0eec753ee1c4e6ae5ac88269aec4b3dc0b7c5e1
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-02-25 15:33:02 +0800

    2.2.3まで課題終了

--- computeCost.m
+++ computeCost.m
@@ -6,14 +6,19 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
+% The result is a vector (h). The vector must be the second term
+% of the multiplication. The number of rows of the vector must equal
+% the number of columns of the matrix. That's why X is first and theta second
+h = X * theta; %// hypothesis function (Theta' * x = theta_0 + theta_1 * x_1)
+squaredErrors = (h - y) .^ 2;
+J = (1 / (2 * m)) * sum(squaredErrors);
 
 
 
